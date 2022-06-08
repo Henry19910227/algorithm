51a70c703922b6f6a75e812c9356b93ccd6a2ebb
Henry.Liao <Henry.Liao@skylinetw.com>
2022-06-08 12:25:18 +0800

    test(LinkList)：加入 LinkList List delete 測試

--- data_structure/link_list/single_link_list_test.go
+++ data_structure/link_list/single_link_list_test.go
@@ -23,3 +23,14 @@ func TestSingleLinkList_Insert(t *testing.T) {
 	linkList.Insert(10, NewNode(100))
 	assert.Equal(t, []int64{5, 10, 15, 20, 30, 100}, linkList.List())
 }
+
+func TestSingleLinkList_Delete(t *testing.T) {
+	linkList := NewSingleLinkList()
+	assert.Error(t, linkList.Delete(100))
+	linkList.Add(NewNode(10))
+	linkList.Add(NewNode(20))
+	linkList.Add(NewNode(30))
+	assert.NoError(t, linkList.Delete(20))
+	assert.Equal(t, []int64{10, 30}, linkList.List())
+	assert.Error(t, linkList.Delete(100))
+}
